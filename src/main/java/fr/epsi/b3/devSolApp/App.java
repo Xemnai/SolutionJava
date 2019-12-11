3cbcb45288c250c2c0d9a981325cd264e24e15d2
Lucas PHELIPOT <l.phelipot@spi.immo>
2019-12-11 09:30:21 +0100

    Creation Kenny

---
+++ src/main/java/fr/epsi/b3/devSolApp/App.java
@@ -0,0 +1,13 @@
+package fr.epsi.b3.devSolApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
