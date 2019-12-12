366c5d19627ee7f64d39cb058820b9bc171d9b74
canneberge <kevfyrezz@gmail.com>
2019-12-12 22:56:05 +0100

    [FEATURE] commencement de la classe Salle

---
+++ src/main/java/fr/epsi/b3/devSolApp/Salle.java
@@ -0,0 +1,12 @@
+package fr.epsi.b3.devSolApp;
+
+import java.util.Date;
+import java.util.List;
+
+public class Salle {
+	
+	private int numero;
+	private boolean isDisponible;
+	private List<Date> dateReserver;
+	
+}
